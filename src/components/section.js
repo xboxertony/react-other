20bf3c5eb2b97e1f8e733b24b0c3f74e82181c4b
Tony Tsai <tonyny58@gmail.com>
2021-06-20 17:29:27 +0800

    uu

--- src/components/section.js
+++ src/components/section.js
@@ -1,8 +1,5 @@
 import React from "react";
 class Section extends React.Component{
-    constructor(props){
-        super(props);
-    }
     render(){
         return <div className="section">歡迎光臨我的頁面</div>;
     }
